9933005a339c4f01f1c236a2d7123f831259f3e6
mtmenji <mtmenji@gmail.com>
2024-07-15 21:37:33 -0600

    w06 code: trees

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -1,42 +1,24 @@
 using System.Collections;
+using System.Collections.Generic;
 
 public class BinarySearchTree : IEnumerable<int> {
     private Node? _root;
 
-    /// <summary>
-    /// Insert a new node in the BST.
-    /// </summary>
     public void Insert(int value) {
-        // Create new node
-        Node newNode = new Node(value);
-        // If the list is empty, then point both head and tail to the new node.
         if (_root is null)
-            _root = newNode;
-        // If the list is not empty, then only head will be affected.
+            _root = new Node(value);
         else
             _root.Insert(value);
     }
 
-    /// <summary>
-    /// Check to see if the tree contains a certain value
-    /// </summary>
-    /// <param name="value">The value to look for</param>
-    /// <returns>true if found, otherwise false</returns>
     public bool Contains(int value) {
         return _root != null && _root.Contains(value);
     }
 
-    /// <summary>
-    /// Yields all values in the tree
-    /// </summary>
     IEnumerator IEnumerable.GetEnumerator() {
-        // call the generic version of the method
         return GetEnumerator();
     }
 
-    /// <summary>
-    /// Iterate forward through the BST
-    /// </summary>
     public IEnumerator<int> GetEnumerator() {
         var numbers = new List<int>();
         TraverseForward(_root, numbers);
@@ -53,9 +35,6 @@ public class BinarySearchTree : IEnumerable<int> {
         }
     }
 
-    /// <summary>
-    /// Iterate backward through the BST.
-    /// </summary>
     public IEnumerable Reverse() {
         var numbers = new List<int>();
         TraverseBackward(_root, numbers);
@@ -65,16 +44,15 @@ public class BinarySearchTree : IEnumerable<int> {
     }
 
     private void TraverseBackward(Node? node, List<int> values) {
-        // TODO Problem 3
+        if (node is not null) {
+            TraverseBackward(node.Right, values);
+            values.Add(node.Data);
+            TraverseBackward(node.Left, values);
+        }
     }
 
-    /// <summary>
-    /// Get the height of the tree
-    /// </summary>
     public int GetHeight() {
-        if (_root is null)
-            return 0;
-        return _root.GetHeight();
+        return _root is null ? 0 : _root.GetHeight();
     }
 
     public override string ToString() {
